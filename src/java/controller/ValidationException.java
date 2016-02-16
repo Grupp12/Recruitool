ac75c976fde692a9f9c1cfff813013a21769478a
Cohdex <jonathan.cohlin@gmail.com>
2016-02-16 15:29:11 +0100

    added javadoc

--- src/java/controller/ValidationException.java
+++ src/java/controller/ValidationException.java
@@ -1,5 +1,8 @@
 package controller;
 
+/**
+ * Exception thrown when data validation fails.
+ */
 public class ValidationException extends Exception {
 	public ValidationException(String message) {
 		super(message);
