3404b8983fcd87abbcea68d010e95f48399bee42
Cohdex <jonathan.cohlin@gmail.com>
2016-02-25 16:01:44 +0100

    added javadoc

--- src/java/model/application/CompetenceProfile.java
+++ src/java/model/application/CompetenceProfile.java
@@ -10,6 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+/**
+ * Contains information about an applicant's
+ * years of experience of one competence.
+ */
 @Entity
 public class CompetenceProfile implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -28,6 +32,13 @@ public class CompetenceProfile implements Serializable {
 	protected CompetenceProfile() {
 	}
 	
+	/**
+	 * Creates a new {@code CompetenceProfile} with the
+	 * specified {@code Competence} and years of experience.
+	 * 
+	 * @param competence
+	 * @param yearsOfExperience 
+	 */
 	public CompetenceProfile(Competence competence, int yearsOfExperience) {
 		this.competence = competence;
 		this.yearsOfExperience = yearsOfExperience;
@@ -41,11 +52,17 @@ public class CompetenceProfile implements Serializable {
 		return yearsOfExperience;
 	}
 	
+	/**
+	 * @inheritDoc
+	 */
 	@Override
 	public int hashCode() {
 		return Long.hashCode(id);
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	@Override
 	public boolean equals(Object object) {
 		if (object == null || !(object instanceof CompetenceProfile))
@@ -56,10 +73,12 @@ public class CompetenceProfile implements Serializable {
 		return id == other.id;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	@Override
 	public String toString() {
 		return String.format("CompetenceProfile[ competence=%s, yearsOfExperience=%d ]",
 				competence, yearsOfExperience);
 	}
-	
 }
