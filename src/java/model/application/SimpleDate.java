9b6dfa79698ac2f74ee58d777983614f94242791
Cohdex <jonathan.cohlin@gmail.com>
2016-02-25 14:45:11 +0100

    added simple date for simple creation of timestamps

---
+++ src/java/model/application/SimpleDate.java
@@ -0,0 +1,35 @@
+package model.application;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * {@code SimpleDate} only serves to provide a constructor for
+ * {@code Timestamp} that takes a {@code String} date in the
+ * format yyy-MM-dd.
+ */
+public class SimpleDate extends Timestamp implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructs a new {@code SimpleDate} set to the date sent as a parameter.
+	 * The date must be in the format yyyy-MM-dd.
+	 * 
+	 * @param date The date in the format yyyy-MM-dd
+	 * @throws ParseException if the date doesn't match the format yyyy-MM-dd
+	 */
+	public SimpleDate(String date) throws ParseException {
+		super(getTimeFromDate(date));
+	}
+	
+	private static long getTimeFromDate(String date) throws ParseException {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		java.util.Date fromDate = dateFormat.parse(date);
+		
+		return fromDate.getTime();
+	}
+}
