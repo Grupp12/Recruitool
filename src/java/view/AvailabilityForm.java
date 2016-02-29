8212dffe8d1a6a37fe4291b0c3e0a0aa87b676ea
simpelito <simonciesluk@gmail.com>
2016-02-29 15:45:54 +0100

    Added a view for submitting applications

---
+++ src/java/view/AvailabilityForm.java
@@ -0,0 +1,25 @@
+package view;
+
+public class AvailabilityForm implements AvailabilityFormDTO{
+	private String from;
+	private String to;
+
+	@Override
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	@Override
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+}
