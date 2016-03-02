080d9b785aee1e810ab97d8612f3ffb49b05d083
snakemodule <Kasmano@gmail.com>
2016-03-02 22:59:00 +0100

    removed unused imports

--- src/java/controller/Controller.java
+++ src/java/controller/Controller.java
@@ -4,23 +4,15 @@ import model.ValidationException;
 import integration.AccountDao;
 import integration.ApplicationDao;
 import integration.EntityExistsException;
-import java.math.BigDecimal;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
 import model.Account;
-import model.Application;
-import model.Availability;
 import model.Competence;
-import model.CompetenceProfile;
-import model.SimpleDate;
 import view.ApplicationFormDTO;
-import view.AvailabilityForm;
-import view.CompetenceProfileForm;
 import view.RegisterFormDTO;
 
 /**
@@ -40,34 +32,41 @@ public class Controller {
 	 *
 	 * @param registerForm the new account info.
 	 * @return the created account.
-	 * 
+	 *
 	 * @throws ValidationException if account data is invalid.
 	 * @throws EntityExistsException if account already exists.
 	 */
 	public Account register(RegisterFormDTO registerForm) throws ValidationException, EntityExistsException {
 		Account acc = new Account(registerForm);
-		
+
 		accountDao.persistAccount(acc);
-		
+
 		return acc;
 	}
-	
+
 	/**
 	 * Create a new application.
-	 * 
+	 *
 	 * @param applicationForm the application info.
-	 * @param account the account associated with the application. 
+	 * @param account the account associated with the application.
 	 * @throws ParseException if wrong date format.
 	 */
 	public void submitApplication(ApplicationFormDTO applicationForm, Account account) throws ParseException {
 		account.createApplication(applicationForm, applicationDao);
 	}
-	
+
+	/**
+	 * @return Gets a list of all available competences
+	 */
 	public List<Competence> getAllCompetences() {
 		return applicationDao.getAllCompetences();
 	}
 
-	public Account retrieveAccount(String currentUser) {
-		return accountDao.getAccount(currentUser);
+	/**
+	 * @param username
+	 * @return Gets the account with the specified username from the database
+	 */
+	public Account retrieveAccount(String username) {
+		return accountDao.getAccount(username);
 	}
 }
